c14cc2ce0047646140ae989fac1275823211e376
MD ABDUR RAHMAN SHOAIB <mdabdurrahmanshoaib@gmail.com>
2026-04-15 16:43:06 +0600

    added dashboard as per figma

---
+++ src/pages/homePage/Dashboard.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { useLoaderData } from "react-router";
+
+const Dashboard = () => {
+  const friends = useLoaderData();
+  return (
+    <div className="mt-10 w-10/12 mx-auto grid grid-cols-12 gap-5 pb-10 border-b border-gray-300">
+      <div className="flex flex-col py-10 px-13 bg-base-200  justify-center items-center rounded-2xl col-span-12 sm:col-span-6 lg:col-span-3">
+        <h4 className="text-4xl mb-2">{friends.length}</h4>
+        <p>Total Friends</p>
+      </div>
+      <div className="flex flex-col py-10 px-13 bg-base-200  justify-center items-center rounded-2xl col-span-12 sm:col-span-6 lg:col-span-3">
+        <h4 className="text-4xl mb-2">3</h4>
+        <p>On Track</p>
+      </div>
+      <div className="flex flex-col py-10 px-13 bg-base-200  justify-center items-center rounded-2xl col-span-12 sm:col-span-6 lg:col-span-3">
+        <h4 className="text-4xl mb-2">6</h4>
+        <p>Need Attention</p>
+      </div>
+      <div className="flex flex-col py-10 px-13 bg-base-200  justify-center items-center rounded-2xl col-span-12 sm:col-span-6 lg:col-span-3">
+        <h4 className="text-4xl mb-2">{friends.length}</h4>
+        <p>Interactions This Month</p>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
