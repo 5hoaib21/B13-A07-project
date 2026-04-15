8a8f51aaf07c0b0dce9a25bb9b7709f2cfaefbf2
MD ABDUR RAHMAN SHOAIB <mdabdurrahmanshoaib@gmail.com>
2026-04-15 16:35:01 +0600

    add a custom error page for (404-error)

---
+++ src/pages/errorPage/ErrorPage.jsx
@@ -0,0 +1,23 @@
+import { Link, useRouteError } from "react-router-dom";
+
+const ErrorPage = () => {
+  const error = useRouteError();
+
+  return (
+    <div className="min-h-screen flex flex-col justify-center items-center text-center bg-base-300 px-4">
+      <h1 className="text-7xl font-bold text-error">Oops!</h1>
+
+      <p className="text-xl mt-2">Something went wrong</p>
+
+      <p className="text-gray-600 mt-2">
+        {error?.statusText || error?.message || "Unknown error"}
+      </p>
+
+      <Link to="/" className="btn btn-primary mt-8">
+        Go Back Home
+      </Link>
+    </div>
+  );
+};
+
+export default ErrorPage;
