9616c054cd3680ed4f57d96854a74a95919fc53c
MD ABDUR RAHMAN SHOAIB <mdabdurrahmanshoaib@gmail.com>
2026-04-15 15:59:08 +0600

    project initialized

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter } from "react-router";
+import RootLayout from "./layout/RootLayout";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: RootLayout,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>{router}</StrictMode>,
+);
